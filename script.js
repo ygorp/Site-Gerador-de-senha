a40c097fc0bd5c8c1a98a67380511d7666eaf7fe
ygorp <ygor-nogueira@hotmail.com>
2022-01-26 21:55:03 -0300

    criando a parte funcional do site

---
+++ script.js
@@ -0,0 +1,23 @@
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button");
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#"
+let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+sliderElement.oninput = function(){
+  sizePassword.innerHTML = this.value;
+}
+
+function generatePassword(){
+
+  let pass = "";
+  for()
+
+
+}
