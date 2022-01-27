2e396b6d0dff35b69203cfe31b88187ec4f87ff1
ygorp <ygor-nogueira@hotmail.com>
2022-01-26 22:16:09 -0300

    finalizado o site gerador de senha

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#"
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#";
 let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
+
 sliderElement.oninput = function(){
   sizePassword.innerHTML = this.value;
 }
@@ -17,7 +18,16 @@ sliderElement.oninput = function(){
 function generatePassword(){
 
   let pass = "";
-  for()
+  for(let i = 0, n = charset.length; i < sliderElement.value; ++i){
+    pass += charset.charAt(Math.floor(Math.random() * n));
+  }
 
+  containerPassword.classList.remove("hide");
+  password.innerHTML = pass;
+  novaSenha = pass;
+}
 
+function copyPassword(){
+  alert("Senha copiada com sucesso!")
+  navigator.clipboard.writeText(novaSenha);
 }
